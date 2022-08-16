43c2fa1a37c3d34402aae9d47df0a31f464ff297
ericalucias <101727981+ericalucias@users.noreply.github.com>
2022-08-16 09:24:58 -0300

    SellerDooJDBC and DAOFactory atu

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,11 @@
-package model.dao;
+package model.dao;  //contem operações estaticas para instanciar os Daos
+
+import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
+	
+	public static SellerDao createSellerDao() { //o createSellerDao retorna o SellerDao, ou seja, quando eu instanciar chamando o createSellerDao ele vai retorna o que esta na inteface SellerDao
+		return new SellerDaoJDBC();
+	}
 
 }
